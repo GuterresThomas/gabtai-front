9c8a41083263aa7b0f5e0e6a3f35efc2ba041cc5
Guterresthomas <guterresmikael@gmail.com>
2024-01-23 13:37:53 -0300

    carrossel ok

--- src/components/Carrossel.tsx
+++ src/components/Carrossel.tsx
@@ -12,10 +12,10 @@ export default function CarrosselComponent() {
     return (
         <div className="flex justify-center">
             <Carousel>
-            <CarouselContent>
-                <CarouselItem><img src="https://picsum.photos/400/300" alt="imagem do carrossel" /></CarouselItem>
-                <CarouselItem><img src="https://picsum.photos/400/300" alt="imagem do carrossel" /></CarouselItem>
-                <CarouselItem><img src="https://picsum.photos/400/300" alt="imagem do carrossel" /></CarouselItem>
+            <CarouselContent className="">
+                <CarouselItem className="w-full m-0 flex justify-center"><img src="https://random.imagecdn.app/500/150" className="w-full object-cover" alt="imagem do carrossel" /></CarouselItem>
+                <CarouselItem className="w-full m-0 flex justify-center"><img src="https://random.imagecdn.app/500/150" className="w-full object-cover" alt="imagem do carrossel" /></CarouselItem>
+                <CarouselItem className="w-full m-0 flex justify-center"><img src="https://random.imagecdn.app/500/150" className="w-full object-cover" alt="imagem do carrossel" /></CarouselItem>
             </CarouselContent>
             <CarouselPrevious className=""/>
             <CarouselNext />
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -17,21 +17,21 @@ import CarrosselComponent from "./Carrossel"
 
 export default function HomePageComponent() {
     return (
-        <div>
-            <div className="w-screen bg-zinc-100 h-20 shadow-md flex items-center justify-between">
+        <div className="w-screen">
+            <div className="w-screen bg-black h-20 shadow-md flex items-center justify-between">
                 <div className="flex items-center">
                     <Avatar>
                         <AvatarImage src="" />
                         <AvatarFallback>Logo</AvatarFallback>
                     </Avatar>
                 </div>
-                <NavigationMenu className="bg-zinc-100">
+                <NavigationMenu>
                     <NavigationMenuList className="">
                         <NavigationMenuItem className="">
-                            <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-800">Cursos</NavigationMenuLink>
-                            <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-800">Módulos</NavigationMenuLink>
-                            <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-800">Professores</NavigationMenuLink>
-                            <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-800">Detalhes</NavigationMenuLink>
+                            <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-100">Cursos</NavigationMenuLink>
+                            <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-100">Módulos</NavigationMenuLink>
+                            <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-100">Professores</NavigationMenuLink>
+                            <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-100">Detalhes</NavigationMenuLink>
                         </NavigationMenuItem>
                     </NavigationMenuList>
                 </NavigationMenu>
