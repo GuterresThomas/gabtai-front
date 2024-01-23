8915f0d1d42b31f83941691ff6b3d027bb4319fc
Guterresthomas <guterresmikael@gmail.com>
2024-01-23 12:04:53 -0300

    NavBar ok

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,39 @@
+'use client'
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+  
+  
+
+
+export default function HomePageComponent() {
+    return (
+        <div className="w-screen bg-zinc-100 h-20 shadow-md flex items-center justify-between">
+            <div className="flex items-center">
+                <Avatar>
+                    <AvatarImage src="" />
+                    <AvatarFallback>Logo</AvatarFallback>
+                </Avatar>
+            </div>
+            <NavigationMenu className="bg-zinc-100">
+                <NavigationMenuList className="">
+                    <NavigationMenuItem className="">
+                        <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-800">Cursos</NavigationMenuLink>
+                        <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-800">Módulos</NavigationMenuLink>
+                        <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-800">Professores</NavigationMenuLink>
+                        <NavigationMenuLink className="m-1 md:m-2 sm:text-xs md:text-xl cursor-pointer hover:font-bold font-medium text-zinc-800">Detalhes</NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
+    )
+
+}
